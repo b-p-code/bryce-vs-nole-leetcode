74d5e43c75321ef7999a806e983cf7308614e5ca
b-p-code <84112025+b-p-code@users.noreply.github.com>
2022-12-12 21:09:40 -0600

    Third max

--- 2047. Number of Valid Words in a Sentence/bryce/Solution.cpp
+++ 2047. Number of Valid Words in a Sentence/bryce/Solution.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    // Statistics are not good
-    // Speed: 32.6%
-    // Memory: 8.13%
     // Apologies Nole, I picked a compiler-specific type of problem
 
     int countValidWords(string s) {
---
+++ 414. Third Maximum Number/bryce/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+		// Actually quite fast
+		// Speed: 66.78%
+		// Memory: 89.30%
+        // Going for clean code
+        // This sorts and then erases unique values
+        sort(nums.begin(), nums.end());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+        // Return the third back or the max
+        if (nums.size() > 2) {
+            return nums[nums.size() - 3];
+        } else {
+            return nums[nums.size() - 1]; 
+        }
+    }
+};
