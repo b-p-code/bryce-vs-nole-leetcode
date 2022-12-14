00705588ad8fcc27f252b7f0cd894f7d386d9526
b-p-code <84112025+b-p-code@users.noreply.github.com>
2022-12-14 13:59:13 -0600

    Updated max binary tree

--- 654. Maximum Binary Tree/bryce/Solution.cpp
+++ 654. Maximum Binary Tree/bryce/Solution.cpp
@@ -9,53 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Pair {
+    public:
+        int index;
+        int value;
+
+        Pair(int index, int value) {
+            this->index = index;
+            this->value = value;
+        }
+};
+
+bool operator<(const Pair&p1, const Pair& p2) {
+    return p1.value < p2.value;
+}
+
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-		// Better solution, still slow
-		// Speed: 5.22%
-		// Memory: 11.59%
+		// Marginally better now
+		// Speed: 15.13%
+		// Memory: 8.24%
+        // THOUGHT PROCESS - we always insert the max value into the tree
+        // where it is positioned within the tree is related to its index
 
-        // Base case
-        if (nums.size() == 0) {
-            return nullptr;
+        TreeNode* root = nullptr;
+
+        priority_queue<Pair> q;
+        for (int i = 0; i < nums.size(); i++) {
+            q.push(Pair(i, nums[i]));
         }
 
-        // Recursively build tree
-        int i = findMax(0, nums.size(), nums);
-        vector<int> left = getSubVector(0, i, nums);
-        vector<int> right = getSubVector(i + 1, nums.size(), nums);
-        TreeNode* root = new TreeNode(
-            nums[i],
-            constructMaximumBinaryTree(left),
-            constructMaximumBinaryTree(right)
-        );
+        while (!q.empty()) {
+            insertNode(root, q.top());
+            q.pop();
+        }
+
+        replaceNodes(root, nums);
 
         return root;
     }
 
-    int findMax(int begin, int end, vector<int> nums) {
-        // Find max
-        int maxIndex = 0;
-        int maxValue = nums[0];
-        for (int i = begin; i < end; i++) {
-            if (nums[i] > maxValue) {
-                maxValue = nums[i];
-                maxIndex = i;
-            }
+    void insertNode(TreeNode* &node, Pair p) {
+        if (node == nullptr) {
+            node = new TreeNode(p.index);
+            return;
+        } else if (node->val < p.index) {
+            insertNode(node->right, p);
+        } else {
+            insertNode(node->left, p);
         }
-
-        return maxIndex;
     }
 
-    vector<int> getSubVector(int begin, int end, vector<int> nums) {
-        vector<int> temp;
-        if (end - begin > nums.size()) {
-            temp.clear();
-            return temp;
+    void replaceNodes(TreeNode* &node, vector<int> nums) {
+        if (node != nullptr) {
+            node->val = nums[node->val];
+            replaceNodes(node->left, nums);
+            replaceNodes(node->right, nums);
+            return;
         }
-
-        temp = vector<int>(nums.begin() + begin, nums.begin() + end);
-        return temp;
     }
 };
